b08775789b5b066871dea4958951bb567f4b4b63
andyrobert3 <alexandermatthew12@gmail.com>
2019-12-24 16:59:49 +0700

    Build navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { logout } from "../actions";
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from "@material-ui/styles";
+
+import SearchBar from './Search';
+
 import Paper from '@material-ui/core/Paper';
 import InputBase from '@material-ui/core/InputBase';
 import IconButton from '@material-ui/core/IconButton';
@@ -9,24 +12,24 @@ import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 
 
-
+// const styles = () => ({
+//   root: {
+//     padding: '2px 4px',
+//     display: 'flex',
+//     alignItems: 'center',
+//     width: 500,
+//     margin: 'auto',
+    
+//   },
+//   input: {
+//     flex: 1
+//   },
+//   iconButton: {
+//     padding: 10,
+//   }
+// });
 
 class Home extends Component {
-  useStyles = () => makeStyles(theme => ({
-    root: {
-      padding: '2px 4px',
-      display: 'flex',
-      alignItems: 'center',
-      width: 400
-    },
-    input: {
-      marginLeft: theme.spacing(1),
-      flex: 1
-    },
-    iconButton: {
-      padding: 10,
-    }
-  }));
 
   handleLogout = () => {
     const { dispatch } = this.props;
@@ -34,28 +37,11 @@ class Home extends Component {
   };
 
   render() {
-    const { isLoggingOut, logoutError } = this.props;
-    const classes = this.useStyles();
+    const { classes, isLoggingOut, logoutError } = this.props;
 
     return (
       <div>
-        <Paper component="form" className={classes.root}>
-          <IconButton className={classes.iconButton} aria-label="menu">
-            <MenuIcon>
-
-            </MenuIcon>
-
-          </IconButton>
-          <InputBase
-            className={classes.input}
-            placeholder="Search podcasts"
-            inputProps={{ 'aria-label': 'search podcasts' }}
-          />
-          <IconButton type="submit" className={classes.iconButton} aria-label="search">
-            <SearchIcon/>
-          </IconButton>      
-        </Paper>
-     
+        <SearchBar></SearchBar>
       
         <h1>This is your app's protected area.</h1>
         <p>Any routes here will also be protected</p>
@@ -73,4 +59,4 @@ function mapStateToProps(state) {
     logoutError: state.auth.logoutError
   };
 }
-export default connect(mapStateToProps)(Home);
+export default (connect(mapStateToProps)(Home));
